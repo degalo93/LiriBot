3b329a5f5c0c30375db0a659706e210b693d2d21
degalo93 <denisgalo10@gmail.com>
2019-03-19 00:06:40 -0700

    added files and worked on liri.js

--- liri.js
+++ liri.js
@@ -4,4 +4,7 @@ require("dotenv").config();
 //movie this
 //do what it says
 
+//spotify id 09ce1018a52d458bba7ab98144ae2596 and sec 13f64a9857394596b56a3c7588420a02
+var Spotify = require('spotify');
+var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
