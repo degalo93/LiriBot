ae254b7780c25284c75c1378b777d66f86f5e10c
degalo93 <denisgalo10@gmail.com>
2019-03-20 23:37:39 -0700

    adding to readme and added images

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # LiriBot
+
+Liri bot is a very simplified version of siri that can only look up song movies and what bands might be in town. This app uses several api and node packages in order to get it working.
+
+It works going into the command line then calling node liri.js and then you have to input one of the comands:
+
+    - concert-this ==> for concert information
+    - spotify-this-song ==> for song information
+    - movie-this ==> for movie information 
+    - do-what-it-says ==> just a default 
+
+Then after you choose you can search for the movie or song that you want and it will pass the information
+
+![movie](images/Capture22.PNG)
--- liri.js
+++ liri.js
@@ -4,76 +4,92 @@ require("dotenv").config();
 //movie this
 //do what it says
 
-
-//spotify id 09ce1018a52d458bba7ab98144ae2596 and sec 13f64a9857394596b56a3c7588420a02
-
-//spotify 
-var Spotify = require('spotify');
-var spotify = new Spotify(keys.spotify);
-
 var axios = require('axios');
 var keys = require("./keys.js");
 var moment = require('moment');
 var fs = require("fs");
+//spotify id 09ce1018a52d458bba7ab98144ae2596 and sec 13f64a9857394596b56a3c7588420a02
+
+//spotify 
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+//({
+//id: '09ce1018a52d458bba7ab98144ae2596',
+//  secret: '13f64a9857394596b56a3c7588420a02'
+//})
+
+
 
 var userChoice = process.argv[2];
 var userSearch = process.argv.slice(3).join(" ");
 
-function userOptions(userChoice, userSearch) {
-    switch (userChoice) {
-        case "spotify-this-song":
-            spotifyThisSong(userSearch);
-            break;
-        case "concert-this":
-            concertThis(userSearch);
-            break;
-        case "movie-this":
-            movieThis(userSearch);
-            break;
-        case "do-this":
-            doThis(userSearch);
-            break
-        default:
-            console.log("Type in spotify-this-song to search for music, conert-this to look for nearby concerts, or movie-this to search for movies");
+switch (userChoice) {
+    case "spotify-this-song":
+        spotifyThisSong(userSearch);
+        break;
+    case "concert-this":
+        concertThis(userSearch);
+        break;
+    case "movie-this":
+        movieThis(userSearch);
+        break;
+    case "do-this":
+        doThis(userSearch);
+        break
+    default:
+        console.log("Type in spotify-this-song to search for music, \n Or concert-this to look for nearby concerts, \n Or movie-this to search for movies \n Or do-this to see an example");
 
-    }
 }
-userOptions(userChoice, userSearch);
+
+
+
+
+function concertThis() {
+    var bandURL = "https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp";
+
+    axios.get(bandURL).then(
+        function(response) {
+            var bandData = response.data;
+            console.log("\nArtist" + userSearch + "\nVenue: " + bandData[0].venue.name + "\nLocation: " + bandData[0].venue.country + "\nDate: " + bandData[0].datatime);
+        });
+}
+
+function movieThis() {
+
+    var URL = "http://www.omdbapi.com/?t=" + userSearch + "&y=&plot=short&apikey=trilogy";
+
+    axios.get(URL).then(
+        function(response) {
+            var movieData = response.data;
+
+            console.log("\n_Movie Info_" + "\nName: " + movieData.Title + "\nRelease Year: " + movieData.Year + "\nRating: " + movieData.Rated + "\nRelease Country: " + movieData.Country + "\nLanguage: " + movieData.Language + "\nPlot: " + movieData.Plot + "\nActors: " + movieData.Actors + "\n");
+
+
+        }
+    );
+}
 
 function spotifyThisSong() {
 
-    console.log("searching for your song");
 
     spotify.search({ type: 'track', query: userSearch }, function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log("\n_Track Info_" + "\nArtist: " + data.tracks.items[0].artists[0].name + "\nSong: " + data.tracks.items[0].name + "\nLink: " + data.tracks.items[0].external_urls.spotify + "\nAlbum: " + data.tracks.items[0].album.name + "\n" + "\nGreat song! Search another :)")
+        console.log("\n_Track Info_" + "\nArtist: " + data.tracks.items[0].artists[0].name + "\nSong: " + data.tracks.items[0].name + "\nLink: " + data.tracks.items[0].external_urls.spotify + "\nAlbum: " + data.tracks.items[0].album.name)
 
     });
 };
 
-function concertThis(artist) {
-    var bandURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+function doThis() {
+    fs.readFile('random.txt', "utf8", function(error, data) {
+        var data = data.split(",");
 
-    axios.get(bandURL).then(
-            function(response) {
+        if (error) {
+            return console.log(error);
+        }
+        spotifyThisSong();
 
-            }
+    });
 
-            function movieThis(movieName) {
-                if (!movieName) {
-                    movieName = "Mr. Nobody";
-                }
-                var URL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-                axios.get(URL).then(
-                    function(response) {
-                        var movieData = response.data;
-
-                        console.log("\n_Movie Info_" + "\nName: " + movieData.Title + "\nRelease Year: " + movieData.Year + "\nRating: " + movieData.Rated + "\nRelease Country: " + movieData.Country + "\nLanguage: " + movieData.Language + "\nPlot: " + movieData.Plot + "\nActors: " + movieData.Actors + "\n");
-
-
-                    }
-                );
-            }
+};
